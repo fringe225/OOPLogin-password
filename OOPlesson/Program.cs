fec1dd7ad6ca6c22cb19df26bd8da7d01adba9ec
fringe225 <turalka@code.edu.az>
2022-02-10 18:21:48 +0400

    Add project files.

---
+++ OOPlesson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOPlesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("UserName daxil edin: ");
+            string name= Console.ReadLine();
+            Console.WriteLine("Password daxil edin: ");
+            string password = Console.ReadLine();
+            User user = new User(name,password);
+
+            Console.WriteLine($"Username - {user.UserName}");
+            Console.WriteLine($"Password - {user.Password}");
+          
+
+
+        }
+    }
+}
