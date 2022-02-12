22acf26090efcecea833bd321aa7f56060ca97dc
fringe225 <turalka@code.edu.az>
2022-02-12 12:31:17 +0400

    Password Lenght 8

--- OOPlesson/User.cs
+++ OOPlesson/User.cs
@@ -48,35 +48,44 @@ namespace OOPlesson
 
                 while (true)
                 {
-                    bool checkUpper = false;
-                    bool checkLower = false;
-                    bool checkDigit = false;
-                    for (int i = 0; i < value.Length; i++)
+                    if (value.Length < 8)
                     {
-                        if (char.IsUpper(value[i]))
-                        {
-                            checkUpper = true;
-                        }
-                        if (char.IsDigit(value[i]))
-                        {
-                            checkDigit = true;
-                        }
-                        if (char.IsLower(value[i]))
-                        {
-                            checkLower = true;
-                        }
-
-                    }
-                    if (checkUpper && checkLower && checkDigit)
-                    {
-                        _password = value;
-                        break;
-                        
+                        Console.WriteLine("Parol en azi 8 chardan ibaret olmalidir!");
+                        value=Console.ReadLine();
                     }
                     else
                     {
-                        Console.WriteLine("Parolu duzgun daxil edin! ");
-                        value = Console.ReadLine();
+                        bool checkUpper = false;
+                        bool checkLower = false;
+                        bool checkDigit = false;
+                        for (int i = 0; i < value.Length; i++)
+                        {
+                            if (char.IsUpper(value[i]))
+                            {
+                                checkUpper = true;
+                            }
+                            if (char.IsDigit(value[i]))
+                            {
+                                checkDigit = true;
+                            }
+                            if (char.IsLower(value[i]))
+                            {
+                                checkLower = true;
+                            }
+
+                        }
+                        if (checkUpper && checkLower && checkDigit)
+                        {
+                            _password = value;
+                            break;
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Parolu duzgun daxil edin! ");
+                            value = Console.ReadLine();
+                        }
+
                     }
 
 
